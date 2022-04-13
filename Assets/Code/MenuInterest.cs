1d840d546778461cbbe3ca96de149435d8431bb3
Noahwus <noahwus@gmail.com>
2022-04-13 14:50:04 -0700

    Run Scene testing / Menu Buttons and Layout

--- Assets/Code/MenuInterest.cs
+++ Assets/Code/MenuInterest.cs
@@ -6,9 +6,14 @@ public class MenuInterest : MonoBehaviour
 {
     public GameObject cam1;
     public GameObject cam2;
+    public GameObject camMenu;
     private Transform start;
     private Vector3 n;
     private Quaternion o;
+    private bool menu = false;
+
+    public GameObject hsUI;
+    public GameObject menuUI;
 
     public float smoothPos;
     public float smoothRot;
@@ -27,8 +32,16 @@ public class MenuInterest : MonoBehaviour
     {
         //Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));
         Vector3 m = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        n = Vector3.Lerp(cam1.transform.position, cam2.transform.position, m.y);
-        o = Quaternion.Lerp(cam1.transform.rotation, cam2.transform.rotation, m.x);
+        if (menu)
+        {
+            n = camMenu.transform.position;
+        }
+        else
+        {
+            n = Vector3.Lerp(cam1.transform.position, cam2.transform.position, m.y);
+            o = Quaternion.Lerp(cam1.transform.rotation, cam2.transform.rotation, m.x);
+        }
+        
 
 
     }
@@ -42,4 +55,18 @@ public class MenuInterest : MonoBehaviour
     private void PosUpdate() => transform.position = Vector3.Lerp(transform.position, n, smoothPos);
 
     private void RotUpdate() => transform.rotation = Quaternion.Lerp(transform.rotation, o, smoothRot);
+
+    public void hsButton()
+    {
+        menuUI.SetActive(false);
+        hsUI.SetActive(true);
+        menu = true;
+    }
+
+    public void menuBack()
+    {
+        menuUI.SetActive(true);
+        hsUI.SetActive(false);
+        menu = false;
+    }
 }
